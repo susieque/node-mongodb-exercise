adbb748f88a759e96764bf99fd7dba86e31c9200
Susie Riley <susie09rob@gmail.com>
2021-01-06 22:42:45 -0800

    Node MongoDB Example 2

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const MongoClient = require('mongodb').MongoClient; //require mongodb node.js driver, imported in the client object from it.
 const assert = require('assert').strict;
+const dboper = require('./operations'); //access to the 4 methods in operations module.
 
 const url = 'mongodb://localhost:27017/';
 const dbname = 'nucampsite';
@@ -18,24 +19,47 @@ MongoClient.connect(url, { useUnifiedTopology: true }, (err, client) => {
 		assert.strictEqual(err, null);
 		console.log('Dropped Collection', result);
 
-		const collection = db.collection('campsites');
-		//recreated campsite collection by inserting a new document into campsites collection.
-		collection.insertOne(
+		dboper.insertDocument(
+			db, //Calling Insert Doc function,
 			{ name: 'Breadcrumb Trail Campground', description: 'Test' },
-			(err, result) => {
-				assert.strictEqual(err, null);
-				console.log('Insert Document:', result.ops);
+			'campsites',
+			(result) => {
+				console.log('Insert Document:', result.ops); //but in parameter list for insert doc function we're defining a callback function, which wont be called until later(this is function definition)code in it doesn't run at this point in the code.
 
-				collection.find().toArray((err, docs) => {
-					//collection find method and two array method to console log all documents from campsites collection.
-					assert.strictEqual(err, null);
+				dboper.findDocuments(db, 'campsites', (docs) => {
 					console.log('Found Documents:', docs);
 
-					client.close(); //close the client.
-            });
-        });
+					dboper.updateDocument(
+						db,
+						{ name: 'Breadcrumb Trail Campground' },
+						{ description: 'Update Test Description' },
+						'campsites',
+						(result) => {
+							console.log('Updated Document Count', result.result.nModified);
+
+							dboper.findDocuments(db, 'campsites', (docs) => {
+								console.log('Found Documents:', docs);
+
+								dboper.removeDocument(
+									db,
+									{ name: 'Breadcrumb Trail Campground' },
+									'campsites',
+									(result) => {
+										console.log('Deleted Document Count:', result.deletedCount);
+
+										client.close(); //close the client. Runs at the end of insert doc function when called. Will have access to the result value later.
+									}
+								);
+							});
+						}
+					);
+				});
+			}
+		);
 	});
 });
+
+//Calling a function vs defining a function. With use of callbacks that are definded in line we're mixing up the two. Calling a funciton is different from defining that function, when you define you're saying this is what this function will do when its called. When you call it, youre writing the code inside it.
 
 //We handle any errors using node error callback convention, assert core module to stop the application if any errors occure.
 //Pay attention to how the code is structured with a series of nested callback functions.
@@ -43,5 +67,5 @@ MongoClient.connect(url, { useUnifiedTopology: true }, (err, client) => {
 //Then that callback had another callback on line 29 ((err, docs) => {
 //Reason for callbacks like this because working with asynchronous operations.
 //When commmunicating between Node application and MongoDB server this is not a synchronous operation.
-//It takes time for that communication to happen, no matter how small. 
+//It takes time for that communication to happen, no matter how small.
 //This kind of callback nesting isn't something we really want to do, check out why in the upcoming exercise.
