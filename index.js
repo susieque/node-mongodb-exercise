7ef1daa3d1212cbff92460951b41122785f5e2d3
Susie Riley <susie09rob@gmail.com>
2021-01-07 01:50:40 -0800

    Node Callback Hell and Promises

--- index.js
+++ index.js
@@ -1,71 +1,54 @@
 const MongoClient = require('mongodb').MongoClient; //require mongodb node.js driver, imported in the client object from it.
-const assert = require('assert').strict;
 const dboper = require('./operations'); //access to the 4 methods in operations module.
 
 const url = 'mongodb://localhost:27017/';
 const dbname = 'nucampsite';
 
-MongoClient.connect(url, { useUnifiedTopology: true }, (err, client) => {
-	//We used that client objects connect method to connect to mongodb server. Connect methods callback gave a client object used to access new campsite database.
-
-	assert.strictEqual(err, null);
-
+MongoClient.connect(url, { useUnifiedTopology: true }).then(client => { //MongoClient.connect method returns a promise. If it fails it gets caught in the catch method at the bottom. If connects, inside connect method will first try to drop campsites connection if it exists. It will console.log the collection was dropped. If rejected error console.log No collection to drop
+	
 	console.log('Connected correctly to server');
 
 	const db = client.db(dbname); //new campsite database
 
-	db.dropCollection('campsites', (err, result) => {
-		//deleted or dropped campsites collection from database.
-		assert.strictEqual(err, null);
+	db.dropCollection('campsites') //this method now returns a promise
+	.then(result => {
 		console.log('Dropped Collection', result);
+	})
+	.catch(err => console.log('No collection to drop.'));  //chain to this promise it's own personal catch block, if a collection named campsites doesn't exist in this database, this promise will return error that we can catch. But not important error, don't want to stop rest of application from running so console.log about this error. But we wont close the connection.
 
-		dboper.insertDocument(
-			db, //Calling Insert Doc function,
-			{ name: 'Breadcrumb Trail Campground', description: 'Test' },
-			'campsites',
-			(result) => {
-				console.log('Insert Document:', result.ops); //but in parameter list for insert doc function we're defining a callback function, which wont be called until later(this is function definition)code in it doesn't run at this point in the code.
+	dboper.insertDocument(db, {name: "Breadcrumb Trail Campground", description: "Test"}, 'campsites')
+	.then(result => {
+        console.log('Insert Document:', result.ops);
 
-				dboper.findDocuments(db, 'campsites', (docs) => {
-					console.log('Found Documents:', docs);
+		return dboper.findDocuments(db, 'campsites');
+	})
+	.then(docs => {
+		console.log('Found Document:', docs);
 
-					dboper.updateDocument(
-						db,
-						{ name: 'Breadcrumb Trail Campground' },
-						{ description: 'Update Test Description' },
-						'campsites',
-						(result) => {
-							console.log('Updated Document Count', result.result.nModified);
+		return dboper.updateDocument(db, { name: 'Breadcrumb Trail Campground' },
+			{ description: 'Update Test Description' }, 'campsites');
+	})
+	.then(result => {
+		console.log('Updated Document Count', result.result.nModified);
 
-							dboper.findDocuments(db, 'campsites', (docs) => {
-								console.log('Found Documents:', docs);
+		return dboper.findDocuments(db, 'campsites'); 
+	})
+	.then(docs => {
+		console.log('Found Documents:', docs);
 
-								dboper.removeDocument(
-									db,
-									{ name: 'Breadcrumb Trail Campground' },
-									'campsites',
-									(result) => {
-										console.log('Deleted Document Count:', result.deletedCount);
+		return dboper.removeDocument(db, { name: 'Breadcrumb Trail Campground' },
+			'campsites');
+		
+	})
+	.then(result => {
+		console.log('Deleted Document Count:', result.deletedCount);
 
-										client.close(); //close the client. Runs at the end of insert doc function when called. Will have access to the result value later.
-									}
-								);
-							});
-						}
-					);
-				});
-			}
-		);
+		return client.close(); //close the client. Runs at the end of insert doc function when called. Will have access to the result value later.
+	})
+	.catch(err => {
+		console.log(err);
+		client.close();
 	});
-});
 
-//Calling a function vs defining a function. With use of callbacks that are definded in line we're mixing up the two. Calling a funciton is different from defining that function, when you define you're saying this is what this function will do when its called. When you call it, youre writing the code inside it.
-
-//We handle any errors using node error callback convention, assert core module to stop the application if any errors occure.
-//Pay attention to how the code is structured with a series of nested callback functions.
-//Line 16 err, result callback function used a method (line 23-25) that required a callback function (err, result) =>(line 25).
-//Then that callback had another callback on line 29 ((err, docs) => {
-//Reason for callbacks like this because working with asynchronous operations.
-//When commmunicating between Node application and MongoDB server this is not a synchronous operation.
-//It takes time for that communication to happen, no matter how small.
-//This kind of callback nesting isn't something we really want to do, check out why in the upcoming exercise.
+})
+.catch(err => console.log(err));
--- operations.js
+++ operations.js
@@ -1,40 +1,21 @@
-const assert = require('assert').strict;
-
-exports.insertDocument = (db, document, collection, callback) => {
-	//Will be called at the end of each method.
-	const coll = db.collection(collection); //in body for this method declare a constant variable named coll(colletion). Collection arguement will be string(like campsites as a string).
-	//use that string as an arguement in db.collection method to obtain reference to collection named campsites thats stored in mongodb server.
-	//can use coll constant to interact w/a specific collection in mongodb server.
-	coll.insertOne(document, (err, result) => {
-		//insertOne & collection methods interact with the database (part of mongoDB node driver API). insertOne takes 2 arguments-document(form of a JS object)2nd is callback function that define inline with 2 parameters(err, result)
-		assert.strictEqual(err, null);
-		callback(result); //callback from somewhere else in the code. Where a call to this insert doc is made. From index.js file
-	});
+exports.insertDocument = (db, document, collection) => {
+	const coll = db.collection(collection); 
+	return coll.insertOne(document); //insertOne will return a promise as its return value. Return will pass that same promise as the return value for the insertDocument method.
 };
 
-exports.findDocuments = (db, collection, callback) => {
+exports.findDocuments = (db, collection) => {
     const coll = db.collection(collection);
-    coll.find().toArray((err, docs) => {
-        assert.strictEqual(err, null);
-        callback(docs);
-    });
+    return coll.find().toArray();
 };
 
 
-exports.removeDocument = (db, document, collection, callback) => {
+exports.removeDocument = (db, document, collection) => {
     const coll = db.collection(collection);
-    coll.deleteOne(document, (err, result) => {
-        assert.strictEqual(err, null);
-        callback(result);
-    });
+    return coll.deleteOne(document);
 };
 
-exports.updateDocument = (db, document, update, collection, callback) => {
+exports.updateDocument = (db, document, update, collection) => {
 	const coll = db.collection(collection);
-	coll.updateOne(document, { $set: update }, null, (err, result) => {
-		//updateOne has 4 parameters. document, info about updates to make(update operator to pass to mongodb) its $set with the update object. To write over existing info. 3rd is to pass in vertain optional config that we dont need so pass null.4th is callback function that gives error or result of operation.
-		assert.strictEqual(err, null);
-		callback(result);
-	});
+	return coll.updateOne(document, { $set: update }, null);
 };
 //analogous to the four crud operations. Inserrt being create, find being read, update to update, & remove being delete. 
